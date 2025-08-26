ec052b54e19eee74f492d724480db988a94aced4
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2025-08-26 11:39:35 +0200

    Update Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,3 +71,7 @@ html_codeblock_linenos_style = "table"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_baseurl = "https://theresiaquintes.github.io/teacups/"
+html_theme_options = {
+    "navigation_with_keys": True,
+}
